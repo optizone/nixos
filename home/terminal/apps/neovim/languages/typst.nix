a553427e121f98dbc4d9e157bca0f81c2a4996e1
optizone <ilya.kek.lol.orbidol@gmail.com>
2025-11-29 19:28:49 +0300

    nvim: bugfixes for typst

--- home/terminal/apps/neovim/languages/typst.nix
+++ home/terminal/apps/neovim/languages/typst.nix
@@ -3,11 +3,16 @@
   programs.nvf.settings.config.vim = {
     languages.typst = {
       enable = true;
-      extensions.typst-concealer.enable = true;
+      # extensions.typst-concealer.enable = true;
       extensions.typst-preview-nvim.enable = true;
       format.enable = true;
       lsp.enable = true;
       treesitter.enable = true;
+    };
+
+    formatter.conform-nvim.setupOpts.formatters_by_ft = {
+      typst = [ "typstyle" ];
+      typ = [ "typstyle" ];
     };
   };
 }
