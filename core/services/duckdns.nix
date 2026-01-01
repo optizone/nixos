833a6091f847a14b973585bf188bedfef8deb7b0
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-01-02 02:20:01 +0300

    duckdns: add service

---
+++ core/services/duckdns.nix
@@ -0,0 +1,9 @@
+_: {
+  sops.secrets."duckdns/optizone/token" = { };
+
+  services.duckdns = {
+    enable = true;
+    domains = [ "optizone" ];
+    tokenFile = "/run/secrets/duckdns/optizone/token";
+  };
+}
