33b7369ea0a9bc0a7b787d4d2e55c5e16b641f3d
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-01-08 04:57:49 +0300

    nfs: update config

--- core/services/nfs.nix
+++ core/services/nfs.nix
@@ -1,21 +1,26 @@
-{ ... }:
-{
-  services.nfs.server.enable = true;
+_: {
+  services.nfs.server = {
+    enable = true;
 
-  # TODO: !!!CRITICAL!!! add some security please )):
-  services.nfs.server.exports = ''
-    /export             192.168.68.2(rw,fsid=0,no_subtree_check)
-    /export/backups     192.168.68.2(rw,nohide,insecure,no_subtree_check)
-    /export/disk-images 192.168.68.2(rw,nohide,insecure,no_subtree_check)
-    /export/media       192.168.68.2(rw,nohide,insecure,no_subtree_check)
-  '';
+    # TODO: uid guid
+    exports = ''
+      /export              192.168.68.0/22(rw,fsid=0,no_subtree_check)
+      /export/backups      192.168.68.0/22(rw,nohide,insecure,no_subtree_check)
+      /export/disk-images  192.168.68.0/22(rw,nohide,insecure,no_subtree_check)
+      /export/media        192.168.68.0/22(rw,nohide,insecure,no_subtree_check)
+      /export/kiwix-images 192.168.68.0/22(rw,nohide,insecure,no_subtree_check)
+    '';
+  };
 
-  networking.firewall.allowedTCPPorts = [
-    111
-    2049
-  ];
-  networking.firewall.allowedUDPPorts = [
-    111
-    2049
-  ];
+  networking.firewall = {
+    allowedTCPPorts = [
+      111
+      2049
+    ];
+
+    allowedUDPPorts = [
+      111
+      2049
+    ];
+  };
 }
