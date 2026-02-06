bbcc4a68c709238f36a3e0978fbadb9d3ad59104
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-02-06 22:22:12 +0300

    bash: add shell aliases

--- home/terminal/shells/bash.nix
+++ home/terminal/shells/bash.nix
@@ -5,5 +5,32 @@ _: {
     initExtra = ''
       macchina -t theme -o host -o kernel -o distribution -o packages -o processor -o memory
     '';
+
+    shellAliases = {
+      "cd" = "z";
+      "cd.." = "cd ..";
+      "cd,," = "cd ..";
+
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../../";
+      "....." = "cd ../../../../";
+
+      ",," = "..";
+      ",,," = "...";
+      ",,,," = "....";
+      ",,,,," = ".....";
+
+      "ct" = "clean-trash-bin";
+      "ns" = "NIXPKGS_ALLOW_UNFREE=1 nix-shell -p";
+      "nd" = "NIXPKGS_ALLOW_UNFREE=1 nix develop";
+      "ndl" =
+        "git reset -- flake.nix flake.lock && git add --intent-to-add flake.nix flake.lock && nd && git update-index --assume-unchanged flake.nix flake.lock";
+      "ndhide" =
+        "git add --intent-to-add flake.nix flake.lock &&
+        git update-index --assume-unchanged flake.nix flake.lock";
+      "nv" = "nvim .";
+    };
+
   };
 }
