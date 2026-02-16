400cfbd06f3228542b345a12165977a210e55f48
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-02-16 21:00:12 +0300

    networking: add rpi5-k host to thinkpad's `/etc/hosts`

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -61,6 +61,10 @@
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
+  networking.hosts = {
+    "192.168.68.118" = [ "rpi5-k" ];
+  };
+
   # needed for MKD2 work project
   systemd.tmpfiles.rules = [
     "d /var/run/protei-config-manager 0777 ${username} users"
