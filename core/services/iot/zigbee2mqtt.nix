014dff9c2df7d6233d7c6cb094fbd805df5e3e03
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-02-28 02:54:41 +0300

    zigbee2mqtt: add settings

--- core/services/iot/zigbee2mqtt.nix
+++ core/services/iot/zigbee2mqtt.nix
@@ -3,7 +3,57 @@ _: {
     enable = true;
 
     dataDir = "/var/lib/zigbee2mqtt";
-    # TODO: copy from imperative
-    settings = { };
+
+    settings = {
+      serial = {
+        port = "/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_225dd3734053ef11a5602de0174bec31-if00-port0";
+        adapter = "ember";
+      };
+
+      mqtt = {
+        base_topic = "zigbee2mqtt";
+        server = "mqtt://localhost:1883";
+      };
+
+      advanced = {
+        channel = 11;
+        network_key = [
+          187
+          229
+          185
+          216
+          152
+          156
+          41
+          114
+          190
+          168
+          42
+          193
+          228
+          208
+          76
+          68
+        ];
+        pan_id = 663;
+        ext_pan_id = [
+          13
+          236
+          94
+          86
+          107
+          239
+          248
+          59
+        ];
+      };
+
+      frontend = {
+        enabled = true;
+        port = 10801;
+      };
+
+      homeassistant.enabled = true;
+    };
   };
 }
