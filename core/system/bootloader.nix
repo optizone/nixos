2ed2cb8e421ab12e9099773c910a2ef738625e6d
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-03-19 18:19:58 +0300

    core: change to kernel zen -> latest

--- core/system/bootloader.nix
+++ core/system/bootloader.nix
@@ -3,7 +3,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.supportedFilesystems = [ "ntfs" ];
   # boot.initrd.kernelModules = [ "i915" ];
 }
