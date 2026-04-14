5c701389883ac4c7df775830b9a0903594da1f93
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-04-15 01:10:12 +0300

    ly: disable session log

--- core/graphics/default.nix
+++ core/graphics/default.nix
@@ -8,5 +8,11 @@
     ./program.nix
   ];
 
-  services.displayManager.ly.enable = true;
+  services.displayManager.ly = {
+    enable = true;
+
+    settings = {
+      session_log = null;
+    };
+  };
 }
