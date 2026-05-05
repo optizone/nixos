9141aee57f5ed1270a82c8d4108ba42b53d2b89e
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-05-05 16:52:51 +0300

    thinkpad: add `keep-outputs` and `keep-derivations` options

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -33,6 +33,9 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
+
+    keep-outputs = true;
+    keep-derivations = true;
   };
 
   nixpkgs.config.allowUnfree = true;
