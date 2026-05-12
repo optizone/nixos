d719ae7ffed5614f7c6323a8b5960ce1b7e10cdb
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-05-12 15:19:00 +0300

    thinkpad: add nixos-raspberrypi.cachix.org substitute for local building

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -28,10 +28,12 @@
     substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
+      "https://nixos-raspberrypi.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
     ];
 
     keep-outputs = true;
