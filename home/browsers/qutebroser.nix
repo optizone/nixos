7807594b51a39e183cc32a5afe464f3309b24cd1
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-05-21 23:22:23 +0300

    qutebrowser: rename `nixpkgs -> np; nixoptions -> no` search engines

--- home/browsers/qutebroser.nix
+++ home/browsers/qutebroser.nix
@@ -6,8 +6,8 @@
     searchEngines = {
       g = "https://www.google.com/search?q={}";
       d = "https://duckduckgo.com/?q={}";
-      nixpkgs = "https://search.nixos.org/packages?query={}";
-      nixoptions = "https://search.nixos.org/options?query={}";
+      np = "https://search.nixos.org/packages?query={}";
+      no = "https://search.nixos.org/options?query={}";
     };
 
     settings = {
