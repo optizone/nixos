2a09afbca48b9bde51dd238924580424a6695bd7
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-05-26 01:43:08 +0300

    rpi4-f: update hardware-config

--- hosts/rpi4-f/hardware-configuration.nix
+++ hosts/rpi4-f/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "usb_storage" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usb_storage" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
