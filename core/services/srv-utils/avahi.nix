c8dabc0bf8086149a5560a5c7d9b662c3ca35e56
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-05-28 15:25:37 +0300

    avahi: add service

---
+++ core/services/srv-utils/avahi.nix
@@ -0,0 +1,15 @@
+{ domain, host, ... }:
+{
+  services.avahi = {
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+
+    enable = true;
+    hostName = host;
+    nssmdns4 = true;
+    openFirewall = true;
+    domainName = domain;
+  };
+}
