c1d0533b67477bbd018f7e0a12a45cb9bdde4272
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-07-01 20:25:15 +0300

    rmpc: theme fixes after update

--- home/terminal/apps/rmpc/default.nix
+++ home/terminal/apps/rmpc/default.nix
@@ -1,6 +1,5 @@
-{ pkgs, ... }:
-{
-  home.packages = [ pkgs.rmpc ];
+{pkgs, ...}: {
+  home.packages = [pkgs.rmpc];
 
   xdg.configFile."rmpc/config.ron".source = ./config.ron;
   xdg.configFile."rmpc/theme.ron".source = ./theme.ron;
