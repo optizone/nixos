32eb5082fa8c504ca9ad5d333530605bc8bb0fa5
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-07-12 20:08:32 +0300

    contact: add `gn02` dev

--- home/terminal/apps/contact.nix
+++ home/terminal/apps/contact.nix
@@ -1,18 +1,13 @@
 {pkgs, ...}: let
   zcontact = pkgs.writeShellScriptBin "zcontact" ''
-    GN01="8C:FD:49:B5:D8:15"
-    GN02="TODO"
-    ST01="ED:96:E1:99:09:20"
-    ST02="C7:80:90:01:30:2E"
-
     OPTIONS=$'gn01\ngn02\nst01\nst02'
     DEV=$(echo "$OPTIONS" | rofi -dmenu -theme-str 'window {width: 10%;} listview {columns: 1;}')
 
     case "$DEV" in
-      gn01) BT="$GN01" ;;
-      gn02) BT="$GN02" ;;
-      st01) BT="$ST01" ;;
-      st02) BT="$ST02" ;;
+      gn01) BT="8C:FD:49:B5:D8:15" ;;
+      gn02) BT="E8:F6:0A:CB:1A:09" ;;
+      st01) BT="ED:96:E1:99:09:20" ;;
+      st02) BT="C7:80:90:01:30:2E" ;;
       *) exit 1
     esac
 
