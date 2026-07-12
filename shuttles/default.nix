34fd0542f8744cb5cd739aea65918578ae7e47bb
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-07-13 01:58:22 +0300

    shuttles: remove hack

--- shuttles/default.nix
+++ shuttles/default.nix
@@ -14,9 +14,6 @@ in {
 
   systemd.tmpfiles.rules = [
     "d /var/run/media/${username} 0755 ${username} users"
-    # FIXME: hack around priveleges
-    "d /var/run/media/${username}/9b67994f-39ae-49c5-956b-c9ecdb685f9e 0755 ${username} users"
-    "d /var/run/media/${username}/cbce37c2-e57a-432c-9f0c-a85618d35ca7 0755 ${username} users"
     "L /home/${username}/zroot/shuttles/k1 - - - - /run/media/${username}/9b67994f-39ae-49c5-956b-c9ecdb685f9e"
     "L /home/${username}/zroot/shuttles/k2 - - - - /run/media/${username}/cbce37c2-e57a-432c-9f0c-a85618d35ca7"
   ];
