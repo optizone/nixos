50b10e4ddf85b02a0886b44136492f8b23a3b596
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-07-15 02:00:10 +0300

    contact: add dev arg

--- home/terminal/apps/contact.nix
+++ home/terminal/apps/contact.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: let
   zcontact = pkgs.writeShellScriptBin "zcontact" ''
     OPTIONS=$'gn01\ngn02\nst01\nst02'
-    DEV=$(echo "$OPTIONS" | rofi -dmenu -theme-str 'window {width: 10%;} listview {columns: 1;}')
+    [ -z "$1" ] && \
+      DEV=$(echo "$OPTIONS" | rofi -dmenu -theme-str 'window {width: 10%;} listview {columns: 1;}') || \
+      DEV="$1"
 
     case "$DEV" in
       gn01) BT="8C:FD:49:B5:D8:15" ;;
