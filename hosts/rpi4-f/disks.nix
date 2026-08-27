fbff46ce3796f9524b22d96004058b1b0d7bbcbd
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-08-27 22:17:37 +0300

    rpi4-f: update disks

--- hosts/rpi4-f/disks.nix
+++ hosts/rpi4-f/disks.nix
@@ -94,37 +94,25 @@ _: {
                 mountOptions = ["noatime"];
               };
             };
-
-            persist = {
-              label = "PERSIST";
-              size = "2G";
-
-              content = {
-                type = "filesystem";
-                mountpoint = "/zroot";
-                format = "ext4";
-                mountOptions = ["noatime"];
-              };
-            };
           };
         };
       };
 
-      # usb = {
-      #   type = "disk";
-      #   device = "/dev/disk/by-id/usb-Kingston_DataTraveler_3.0_E0D55EA573F5E38169B20100-0:0";
-      #
-      #   content = {
-      #     type = "btrfs";
-      #     extraArgs = ["-f" "-U" "5ccc0817-2cf4-423e-8782-5e89e6e06569"];
-      #     subvolumes = {
-      #       "/zroot" = {
-      #         mountpoint = "/zroot";
-      #         mountOptions = ["compress=lz4"];
-      #       };
-      #     };
-      #   };
-      # };
+      usb = {
+        type = "disk";
+        device = "/dev/disk/by-id/usb-Kingston_DataTraveler_3.0_E0D55EA573F5E38169B20100-0:0";
+
+        content = {
+          type = "btrfs";
+          extraArgs = ["-f" "-U" "5ccc0817-2cf4-423e-8782-5e89e6e06569"];
+          subvolumes = {
+            "/zroot" = {
+              mountpoint = "/zroot";
+              mountOptions = ["compress=lz4"];
+            };
+          };
+        };
+      };
     };
   };
 }
