39745224a62408eafc6746b93bdd234698a28017
optizone <ilya.kek.lol.orbidol@gmail.com>
2026-09-14 20:33:54 +0300

    meshtastic: refactor helper scripts

--- core/meshtastic/default.nix
+++ core/meshtastic/default.nix
@@ -5,57 +5,109 @@
   ...
 }: let
   zcontact = pkgs.writeShellScriptBin "zcontact" ''
-    OPTIONS=$'gn01\ngn02\nst01\nst02'
+    nodes=$(mnodes -l)
+    options="''${nodes//$' '/$'\n'}"
     [ -z "$1" ] && \
-      DEV=$(echo "$OPTIONS" | rofi -dmenu -theme-str 'window {width: 10%;} listview {columns: 1;}') || \
-      DEV="$1"
+      dev=$(echo "$options" | rofi -dmenu -theme-str 'window {width: 10%;} listview {columns: 1;}') || \
+      dev="$1"
 
-    case "$DEV" in
-      gn01) BT="8C:FD:49:B5:D8:15" ;;
-      gn02) BT="E8:F6:0A:CB:1A:09" ;;
-      st01) BT="ED:96:E1:99:09:20" ;;
-      st02) BT="C7:80:90:01:30:2E" ;;
-      *) exit 1
-    esac
+    [ -z "$dev" ] && exit 0
 
-    contact --ble $BT
+    contact --ble "$(mnodes --ble $dev)"
   '';
 
   mnodes = pkgs.writeShellScriptBin "mnodes" ''
-    node="$1"
+    declare -rA node_to_ble=(
+      [gn01]="8C:FD:49:B5:D8:15"
+      [gn02]="E8:F6:0A:CB:1A:09"
+      [st01]="ED:96:E1:99:09:20"
+      [st02]="C7:80:90:01:30:2E"
+    )
+
+    declare -rA node_to_config=(
+      [gn01]="${builtins.readFile ./nodes/gn01.yaml}"
+      [gn02]="${builtins.readFile ./nodes/gn02.yaml}"
+      [st01]="${builtins.readFile ./nodes/st01.yaml}"
+      [st02]="${builtins.readFile ./nodes/st02.yaml}"
+    )
+
+    usage() {
+      echo "mnodes [--configure|-c <NODE>] [--print|-p <NODE>] [--ble|-b <NODE>] [--list|-l] [--help|-h]"
+      echo
+      echo "  Avaliable nodes: ''${!node_to_ble[@]}"
+      echo
+      echo "  --configure: loads configuration to the provided node"
+      echo "  --print: prints information about provided node"
+      echo "  --ble: prints bluetooth MAC address of the provided node"
+      echo "  --list: prints all avaliable nodes"
+      echo "  --help: prints this message"
+      exit $1
+    }
+
+    while [ $# -gt 0 ]; do
+      case "$1" in
+        "-p" | "--print")
+          [ $# -lt 2 ] && usage 1
+          print=true
+          node="$2"
+          shift 2
+          ;;
+
+        "-b" | "--ble")
+          [ $# -lt 2 ] && usage 1
+          bluetooth=true
+          node="$2"
+          shift 2
+          ;;
+
+        "-c" | "--configure")
+          [ $# -lt 2 ] && usage 1
+          configure=true
+          node="$2"
+          shift 2
+          ;;
+
+        "-h" | "--help")
+          usage 0
+          ;;
+
+        "-l" | "--list")
+          echo "''${!node_to_ble[@]}"
+          exit 0
+          ;;
+
+        *)
+          usage 1
+          ;;
+      esac
+    done
+
+    if [[ ! -v node_to_ble[$node] ]]; then
+      usage 1
+    fi
 
     username=$(cat ${config.sops.secrets."mnodes/mqtt/username".path})
     password=$(cat ${config.sops.secrets."mnodes/mqtt/password".path})
     fixedPin=$(cat ${config.sops.secrets."mnodes/bluetooth-pin".path})
+    channel_url=$(cat ${config.sops.secrets."mnodes/channel-url".path})
 
-    gn01() {
-      privateKey=$(cat ${config.sops.secrets."mnodes/private-key/gn01".path})
-      conf="${builtins.readFile ./nodes/gn01.yaml}"
-      echo "$conf"
-    }
+    ble="''${node_to_ble[$node]}"
+    config="''${node_to_config[$node]}"
+    privateKey=$(cat "/run/secrets/mnodes/private-key/$node")
 
-    gn02() {
-      privateKey=$(cat ${config.sops.secrets."mnodes/private-key/gn02".path})
-      conf='${builtins.readFile ./nodes/gn02.yaml}'
-      echo "$conf"
-    }
-
-    st01() {
-      privateKey=$(cat ${config.sops.secrets."mnodes/private-key/st01".path})
-      conf='${builtins.readFile ./nodes/st01.yaml}'
-      echo "$conf"
-    }
-
-    st02() {
-      privateKey=$(cat ${config.sops.secrets."mnodes/private-key/st02".path})
-      conf='${builtins.readFile ./nodes/st02.yaml}'
-      echo "$conf"
-    }
-
-    [ -z "$node" ] || [ "$node" = "gn01" ] && gn01
-    [ -z "$node" ] || [ "$node" = "gn02" ] && gn02
-    [ -z "$node" ] || [ "$node" = "st01" ] && st01
-    [ -z "$node" ] || [ "$node" = "st02" ] && st02
+    if [ "$configure" = true ]; then
+      tmp=$(mktemp)
+      echo "$config" > tmp
+      meshtastic --configure "$tmp" --ble "$ble"
+      rm "$tmp"
+    elif [ "$print" = true ]; then
+      echo "NODE: $node"
+      echo "BLE: $ble"
+      echo "CONFIG: "
+      echo "$config"
+    elif [ "$bluetooth" = true ]; then
+      echo "$ble"
+    fi
 
   '';
 in {
